e5dd31c1f6000b652795331590a0f5d0cbdef4b0
Bullzai <hardas322@yahoo.com>
2023-06-03 14:21:09 +0100

    Components completed

---
+++ course-app/src/components/course.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import React from "react"
+import Module from "./module"
+
+const Course = (props) => {
+  const list = props.modules.map((module, index) => (
+    <Module key={index} description={module} />
+  ))
+  return (
+    <>
+      <h2>{`${props.title} modules table`} </h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>{list}</tbody>
+      </table>
+    </>
+  )
+}
+
+export default Course
---
+++ course-app/src/components/module.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import React from "react"
+
+const Module = (props) => {
+  return (
+    <tr>
+      <td>{props.description.name}</td>
+      <td>{props.description.noLectures}</td>
+      <td>{props.description.noPracticals}</td>
+    </tr>
+  )
+}
+
+export default Module
