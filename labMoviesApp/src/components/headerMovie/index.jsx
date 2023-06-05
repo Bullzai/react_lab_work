527794b420a704c9f1a778cb59c0b143df1478a3
Bullzai <hardas322@yahoo.com>
2023-06-05 12:44:05 +0100

    Exercise - Show favourite icon on Movie Details page

--- labMoviesApp/src/components/headerMovie/index.jsx
+++ labMoviesApp/src/components/headerMovie/index.jsx
@@ -5,18 +5,25 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Avatar from "@mui/material/Avatar";
+import CardHeader from "@mui/material/CardHeader";
 
 const styles = {
-    root: {  
+  root: {
     display: "flex",
     justifyContent: "space-around",
     alignItems: "center",
     flexWrap: "wrap",
     padding: 1.5,
   },
+  avatar: {
+    backgroundColor: "rgb(255, 0, 0)",
+  },
 };
 
 const MovieHeader = (props) => {
+  const favList = JSON.parse(localStorage.getItem("favourites"));
   const movie = props.movie;
 
   return (
@@ -24,11 +31,21 @@ const MovieHeader = (props) => {
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
-
+      <CardHeader
+        avatar={
+          // iterate over all favourite movie list & see if it matches the movie we're inspecting
+          favList.some((favMovie) => favMovie.id === movie.id) ? (
+            <Avatar sx={styles.avatar}>
+              <FavoriteIcon />
+            </Avatar>
+          ) : null
+        }
+      />
       <Typography variant="h4" component="h3">
-        {movie.title}{"   "}
+        {movie.title}
+        {"   "}
         <a href={movie.homepage}>
-          <HomeIcon color="primary"  fontSize="='large"/>
+          <HomeIcon color="primary" fontSize="='large" />
         </a>
         <br />
         <span>{`${movie.tagline}`} </span>
